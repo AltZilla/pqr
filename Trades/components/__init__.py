b2ab82344afb10f260b16a2c96162df52698381d
AltZilla <87214640+AltZilla@users.noreply.github.com>
2022-08-05 21:14:55 +0530

    possible mentions

--- Trades/components/__init__.py
+++ Trades/components/__init__.py
@@ -1,1 +1,2 @@
 from .embedview import *
+from .possiblementions import *
--- Trades/trades.py
+++ Trades/trades.py
@@ -1,13 +1,73 @@
+import re
 import discord
 
-from redbot.core import commands, Config
-from typing import Literal, Optional, Union
-from .reminder import Reminder
-from .components import EmbedPeekView
 from . import TRADES_GUILD_ID
+from redbot.core import commands, Config
+from discord import app_commands as app
+from fuzzywuzzy import process
+from typing import Literal, Optional, Tuple, Union, List
+from .reminder import Reminder
+from .components import EmbedPeekView, PossibleMentionsView
+
+@app.context_menu(name = "Possible Mentions")
+async def _possible_mentions(interaction: discord.Interaction, message: discord.Message):
+    result: List[Tuple[discord.Object, int]] = []
+    string = message.clean_content
+    await interaction.response.defer(ephemeral=True)
+    if message.embeds:
+        def clean(d):
+            d = list(d)
+            for t in d:
+                if not isinstance(t, str):
+                   d.remove(t)
+            return d
+
+        for embed in map(lambda e: e.to_dict(), message.embeds):
+            for _ in embed.values():
+                if isinstance(_, str):
+                    string += ' ' + _
+                elif isinstance(_, dict):
+                    string += ' ' + ' '.join(clean(_.values()))
+                elif isinstance(_, list):
+                    string += ' ' + ' '.join([' '.join(clean(field.values())) for field in _])
+
+    to_check = message.guild.members + message.guild.roles + [channel for channel in message.guild.channels if channel.permissions_for(interaction.user).view_channel]
+
+    def _check(obj):
+        for item in result:
+            if item[0] == obj:
+               return False
+        return True
+
+    for _id in re.findall(r'(?P<id>[0-9]{15,20})', string):
+        obj_id, score = process.extractOne(_id, map(lambda o: o.id, to_check))
+        obj = discord.utils.get(to_check, id = obj_id)
+        if _check(obj):
+           result.append((obj, score))
+    
+    for _tag in re.findall(r'.{1,50}#[0-9]{4}', string):
+        obj, score = process.extractOne(_tag, message.guild.members)
+        if _check(obj):
+           result.append((obj, score))
+
+    remaining = 10 - len(result)
+
+    if remaining > 0:
+       _extracted = process.extract(string, map(lambda m: m.name, to_check), limit = remaining)
+       for name, score in _extracted:
+           obj = discord.utils.get(to_check, name = name)
+           if _check(obj):
+              result.append((obj, score))
+
+    data = {
+        'members': [r for r in result if isinstance(r[0], discord.Member)][:5],
+        'roles': [r for r in result if isinstance(r[0], discord.Role)][:5],
+        'channels': [r for r in result if any(isinstance(r[0], t) for t in [discord.TextChannel, discord.VoiceChannel, discord.CategoryChannel])][:5]
+    }
+    await PossibleMentionsView(interaction.user, **data).start(interaction)
 
 class Trades(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.config = Config.get_conf(self, identifier = 719180744311701505)
         self.config.register_guild(
@@ -17,6 +77,9 @@ class Trades(commands.Cog):
             vote__reminders = True
         )
         self._reminder = Reminder(self)
+
+        ### for now...
+        self.bot.tree.add_command(_possible_mentions)
 
     async def cog_check(self, ctx: commands.Context) -> bool:
         return ctx.guild.id == TRADES_GUILD_ID
